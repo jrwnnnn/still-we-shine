0e4b3753216f0462af36a4daf3c11bbb856c351d
Mark Jerwin <136870415+jrwnnnn@users.noreply.github.com>
2026-03-01 14:27:38 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 // This is an AstroDB via Turso, deployment ready via adapter-node Astro scaffold.
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import db from "@astrojs/db";
 import node from "@astrojs/node";
 
 // https://astro.build/config
@@ -10,7 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [db()],
   output: 'server',
   adapter: node({
     mode: "standalone"
